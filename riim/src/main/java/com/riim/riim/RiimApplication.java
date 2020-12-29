1e3493050d3454510a6b4b7e42f8f4a188530a19
riim <riim715@naver.com>
2020-12-29 21:59:50 +0900

    upload

---
+++ riim/src/main/java/com/riim/riim/RiimApplication.java
@@ -0,0 +1,13 @@
+package com.riim.riim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiimApplication.class, args);
+	}
+
+}
---
+++ riim/src/test/java/com/riim/riim/RiimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riim.riim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
