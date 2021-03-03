fede6d3b162c95d46117d9a5944c1ac74085cda5
ParkJaeRim <riim715@naver.com>
2021-03-03 14:48:22 +0900

    [Update] 회원가입 페이지 변경 및 로그인 확인 과정 헤더로 이동

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1 v-if="isCookie != null">로그인 됐지렁</h1>
+    
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -10,26 +10,17 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
+
 export default {
   name: "Home",
 
   data() {
     return {
-      isCookie: null,
     };
   },
   components: {
     HelloWorld,
   },
-  created() {
-    this.isCookie = this.getCookie("accessToken");
-  },
-  
-  methods: {
-    getCookie(name) {
-      var value = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
-      return value ? value[2] : null;
-    },
-  },
+
 };
 </script>
--- riim/src/main/java/com/riim/riim/config/security/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.riim.riim.config.security;
-import com.riim.riim.model.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
